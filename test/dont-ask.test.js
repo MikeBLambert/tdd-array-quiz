82f1ff4a0b872b7db065e8e7cd976ae75791d827
MikeBLambert <mlamb128@gmail.com>
2018-10-01 19:32:00 -0700

    Rewrite exam for own personal benefit after Marty's feedback. Note to self: 'Relax and read instructions more carefully on future exams. #Learning

--- test/dont-ask.test.js
+++ test/dont-ask.test.js
@@ -1,16 +1,23 @@
 const assert = require('assert');
-const Replacer = require('../lib/dont-ask.js');
+const replacer = require('../lib/dont-ask.js');
 
 describe('replacer', () => {
 
     it('removes words with "y" from array', done => {
-        const replacer = new Replacer([]);
         
-        const arr = ['sky', 'hot', 'green'];
-        replacer.remove(arr, (err, newArr) => {
-            if(err) return done(err);
-            assert.deepEqual(newArr, ['hot', 'green']);
-            done();
-        });
+        let arr = ['sky', 'hot', 'green', 'Yeti'];
+        const transformed = replacer(arr);
+   
+        assert.deepEqual(transformed.length, 2);
+        done();
+    });
+
+    it('replaces words without "y" with "Is it <word>?"', done => {
+        
+        let arr = ['sky', 'hot', 'green', 'Yeti'];
+        const transformed = replacer(arr);
+   
+        assert.deepEqual(transformed, ['Is it hot?', 'Is it green?']);
+        done();
     });
 });
